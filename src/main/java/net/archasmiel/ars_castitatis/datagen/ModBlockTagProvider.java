462d6e17e056632f4f7e2a458e09fad8b2995a1f
Archasmiel <archasmiel@gmail.com>
2025-04-29 11:43:55 +0300

    0.1.5b
    Add tags datagen

--- src/main/java/net/archasmiel/ars_castitatis/datagen/ModBlockTagProvider.java
+++ src/main/java/net/archasmiel/ars_castitatis/datagen/ModBlockTagProvider.java
@@ -1,8 +1,10 @@
 package net.archasmiel.ars_castitatis.datagen;
 
 import net.archasmiel.ars_castitatis.ArsCastitatisMod;
+import net.archasmiel.ars_castitatis.block.ModBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 
 import java.util.concurrent.CompletableFuture;
@@ -16,7 +18,20 @@ public class ModBlockTagProvider extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE)
+            .replace(false)
+            .add(ModBlocks.REINFORCED_DIORITE.get())
+            .add(ModBlocks.PAPER_BLOCK.get())
+            .add(ModBlocks.MAGIC_BRICKS.get());
 
+        tag(BlockTags.NEEDS_DIAMOND_TOOL)
+            .replace(false)
+            .add(ModBlocks.REINFORCED_DIORITE.get());
+
+        tag(BlockTags.NEEDS_IRON_TOOL)
+            .replace(false)
+            .add(ModBlocks.PAPER_BLOCK.get())
+            .add(ModBlocks.MAGIC_BRICKS.get());
     }
 
 }
--- src/main/java/net/archasmiel/ars_castitatis/datagen/ModItemTagProvider.java
+++ src/main/java/net/archasmiel/ars_castitatis/datagen/ModItemTagProvider.java
@@ -1,9 +1,11 @@
 package net.archasmiel.ars_castitatis.datagen;
 
 import net.archasmiel.ars_castitatis.ArsCastitatisMod;
+import net.archasmiel.ars_castitatis.tags.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 
 import java.util.concurrent.CompletableFuture;
@@ -17,7 +19,10 @@ public class ModItemTagProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-
+        tag(ModTags.Items.TRANSFORMABLE_ITEMS)
+            .add(Items.DIAMOND)
+            .add(Items.GOLD_INGOT)
+            .add(Items.COPPER_INGOT);
     }
 
 }
